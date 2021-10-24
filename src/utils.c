1e298dc05ae0dfecd480fbc5499b74bbde725e12
DallasSchauer <60755082+DallasSchauer@users.noreply.github.com>
2021-10-24 02:11:03 -0500

    searchPatternInFile - not perfect, but functional
    Function searchPatternInFile in utils. Directory traversal is messed up so can't test all files, but appears to work correctly when called by master.

--- src/utils.c
+++ src/utils.c
@@ -5,7 +5,8 @@
 #include<string.h>
 #include "utils.h"
 
-int STRING_BUFFER = 100;
+const int STRING_BUFFER = 100;
+const int WRITE_FD = STDOUT_FILENO;
 
 /*
 	Searches for pattern in file and writes the entire line containing the pattern to STDOUT
@@ -16,7 +17,7 @@ int STRING_BUFFER = 100;
 */
 void searchPatternInFile(char* path, char* pattern){
 
-	
+
 	FILE * fp;
 
 	fp = fopen(path, "r");
@@ -24,10 +25,24 @@ void searchPatternInFile(char* path, char* pattern){
 		fprintf(stderr,"Error opening file: %s \n",path);
 		return;
 	}
-	
+
+	char buffer[STRING_BUFFER];
+	char * line = (char*)malloc(sizeof(char)*STRING_BUFFER);
+	char *filePath = (char*) malloc(sizeof(char) * STRING_BUFFER);
+
+	while (fgets(buffer, STRING_BUFFER, fp) != NULL) {
+		if((line = strstr(buffer, pattern)) != NULL) {
+				if (*(line-1) == ' ' || (strcmp(buffer, line) == 0)) //make sure the substring is actually a word and not just part of a word.
+				{
+						printf("%s: %s\n", path, buffer);
+				}
+		}
+	}
+
 	//Read file line by line and check if pattern exists in the line
-		
-		
+
 	//Close fp
+	//free(line);                // these frees cause an error for some reason in run3?
+	//free(filePath);
 	fclose(fp);
 }
